9e59383738428655e5f1d5abbb7a029d464f6411
Jappe02 <jasper.mannonen@gmail.com>
2026-01-17 01:34:24 +0200

    Add exports for network model classes

--- lib/jappeos_services.dart
+++ lib/jappeos_services.dart
@@ -7,3 +7,6 @@ export 'src/services/power_manager/power_manager_service.dart';
 export 'src/services/session_manager/session_manager_service.dart';
 export 'src/services/account_manager/account_manager_service.dart';
 export 'src/services/network_manager/network_manager_service.dart';
+export 'src/services/network_manager/model/network_connection.dart';
+export 'src/services/network_manager/model/network_device.dart';
+export 'src/services/network_manager/model/wifi_access_point.dart';
